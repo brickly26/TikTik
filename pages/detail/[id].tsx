8382141aa5d44c878c3f84b4e2987bfa02ce39f7
brickly26 <sabburak26@gmail.com>
2022-08-06 12:15:33 -0400

    added comment feature on post details page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -25,6 +25,8 @@ const Detail = ({ postDetails }: IProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const router = useRouter();
   const { userProfile }: any = useAuthStore();
+  const [comment, setComment] = useState('');
+  const [isPostingComment, setIsPostingComment] = useState(false);
 
   const onVideoClick = () => {
     if (playing) {
@@ -51,6 +53,24 @@ const Detail = ({ postDetails }: IProps) => {
       })
 
       setPost({ ...post, likes: data.likes});
+    }
+  }
+
+  const addComment = async (e) => {
+    e.preventDefault();
+
+    if(userProfile && comment) {
+      setIsPostingComment(true);
+
+      const { data } = await axios.put(`${BASE_URL}/api/post/${post._id}`, 
+      {
+        userId: userProfile._id,
+        comment : comment.trim(),
+      });
+
+      setPost({ ...post, comments: data.comments });
+      setComment('');
+      setIsPostingComment(false);
     }
   }
 
@@ -142,7 +162,11 @@ const Detail = ({ postDetails }: IProps) => {
           </div>
 
           <Comments 
-
+            comment={comment}
+            setComment={setComment}
+            addComment={addComment}
+            comments={post.comments}
+            isPostingComment={isPostingComment}
           />
 
         </div>
